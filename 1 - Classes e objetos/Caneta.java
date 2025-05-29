6a9f69f564f268366cf928a9c6376f97c822d0aa
Eric Rian <eric.riaan@gmail.com>
2025-05-28 22:38:58 -0300

    poo, obj, class

---
+++ 1 - Classes e objetos/Caneta.java
@@ -0,0 +1,26 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    Boolean tampada;
+
+    void status(){
+        System.out.println("uma caneta " + this.cor);
+        System.out.println(this.tampada);
+    }
+    void rabiscar(){
+if(this.tampada == true){
+System.out.println("caneta tampada");
+}else {
+    System.out.println("rabiscando");
+    }}
+    void tampar(){
+        this.tampada = true;
+
+    }
+    void destampar(){
+        this.tampada = false;
+
+    }
+}
---
+++ 1 - Classes e objetos/Conceitos.java
@@ -0,0 +1,6 @@
+public class Conceitos {
+    /*Classes são os moldes do objeto
+    Objeto faz algo e tem características(valores)
+    Atributos são como varáveis
+    Métodos são como funções*/
+}
